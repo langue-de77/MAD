34e6a4393215efc86db0478d44087881d58cff2c
Riku Matsui <184587145+langue-de77@users.noreply.github.com>
2025-09-02 13:28:47 +0900

    add Button

--- app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s241205161;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,8 +27,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.changeButton.setOnClickListener(view -> {
+            var color = (-1 * (binding.colorText.getCurrentTextColor())+1600000) % 16777216;
+            if (color > 0)
+                color = color * -1;
+            binding.colorText.setText(String.valueOf(color));
+            binding.colorText.setTextColor(color);
+        });
+        binding.applyButton.setOnClickListener(view -> {
+            binding.text.setText(binding.editText.getText().toString() + ": Applied");
+        });
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
+
         binding.text.setText(R.string.author_lastName);
         binding.text.setTextColor(binding.text.getCurrentTextColor()+100);
-        binding.image.setImageResource(R.drawable.outline_android_24);
     }
 }
