df2917a8addf3449df83d4f6ea0f2de673cd3a24
Riku Matsui <184587145+langue-de77@users.noreply.github.com>
2025-09-02 11:59:34 +0900

    add image

--- app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity.java
@@ -7,21 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.TextView;
+
+import jp.ac.meijou.android.s241205161.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView text = findViewById(R.id.text);
-        text.setText(R.string.author_lastName);
+        binding.text.setText(R.string.author_lastName);
+        binding.text.setTextColor(binding.text.getCurrentTextColor()+100);
+        binding.image.setImageResource(R.drawable.outline_android_24);
     }
 }
