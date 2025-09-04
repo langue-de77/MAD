1e31dd8f6ad8a06170055922d96aa23b14165b63
Riku Matsui <184587145+langue-de77@users.noreply.github.com>
2025-09-04 14:29:46 +0900

    add NetworkChecker(MainActivity7)

--- app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity7.java
+++ app/src/main/java/jp/ac/meijou/android/s241205161/MainActivity7.java
@@ -1,5 +1,9 @@
 package jp.ac.meijou.android.s241205161;
 
+import android.net.ConnectivityManager;
+import android.net.LinkAddress;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +12,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.stream.Collectors;
+
+import jp.ac.meijou.android.s241205161.databinding.ActivityMain7Binding;
+
 public class MainActivity7 extends AppCompatActivity {
+
+    private ActivityMain7Binding binding;
+    private ConnectivityManager connectivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main7);
+        binding = ActivityMain7Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        connectivityManager = getSystemService(ConnectivityManager.class);
+        var currentNetwork = connectivityManager.getActiveNetwork();
+        updateTransport(currentNetwork);
+        updateIpAddress(currentNetwork);
+    }
+    private void updateTransport(Network network){
+        var caps = connectivityManager.getNetworkCapabilities(network);
+        if (caps != null){
+            String transport;
+            if(caps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+                transport = "モバイル通信";
+            else if(caps.hasTransport(NetworkCapabilities.TRANSPORT_WIFI))
+                transport = "WiFi";
+            else
+                transport = "その他";
+            binding.transportText.setText("Transport : "+transport);
+        }
+    }
+    private void updateIpAddress(Network network){
+        var linkProperties = connectivityManager.getLinkProperties(network);
+        if(linkProperties != null){
+            var address = linkProperties.getLinkAddresses().stream()
+                    .map(LinkAddress::toString)
+                    .collect(Collectors.joining("\n"));
+
+            binding.ipText.setText("IPAddress : \n"+address);
+        }
     }
 }
